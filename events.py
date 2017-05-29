fbd953815f6f9f552269231bd7b1ef67efe3f93b
Ryan Prairie <prairie421@gmail.com>
2017-05-28 20:47:20 -0400

    object reworked to work with creator,title,time,place

--- events.py
+++ events.py
@@ -3,17 +3,14 @@ class EventList(object):
     """ an object that is an individual event to keep track of the time, place, and the
         reason for the event"""
 
-    def __init__(self, creator, time, place, reason):
-
+    def __init__(self, creator, title, time, place):
+        self.creator = creator
+        self.title = title
         self.time = time
         self.place = place
-        self.reason = reason
-        self.creator = creator
 
     def printDetails(self):
-        
-        print(self.creator)
-        print(self.place)
-        print(self.time)
-        print(self.reason)
-
+        print("creator: {}".format(self.creator))
+        print("title: {}".format(self.title))
+        print("time: {}".format(self.time))
+        print("place: {}".format(self.place))
