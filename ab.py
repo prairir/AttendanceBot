8ac97e8b4b52c012dba95c82586e3ed6ce9eafad
Ryan Prairie <prairie421@gmail.com>
2017-05-28 20:46:29 -0400

    events done, attendance needs to be added

--- ab.py
+++ ab.py
@@ -17,13 +17,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!addevent'):
-        events.append(EventList(message.author, message.content[9:],))
+    if message.content.startswith('!AddEvent'):
+        delims = 'title: ', ' time: ', ' place: '
+        fields = regsplit(delims, message.content[10:])
+        events.append(EventList(message.author, fields[1], fields[2], fields[3]))
 
     if message.content.startswith('!print'):
         for i in events:
             i.printDetails()
             print("-----------------")
+    
+
+def regsplit(delimitors, string, maxsplit=0):
+    import re 
+    regexPattern = '|'.join(map(re.escape, delimitors))
+    return re.split(regexPattern, string, maxsplit)
 
 
 
